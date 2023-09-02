69a024ed1fbb396e75ed85d4ee8549e264f4c343
mahmoud khames <khamesh680@gmail.com>
2023-09-02 21:03:11 +0300

    move love

--- src/index.js
+++ src/index.js
@@ -5,17 +5,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
  import {store} from './Redux/Store';
 import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
     <Provider store={store}> 
         <App />
      </Provider> 
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
